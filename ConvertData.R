a3e8c7a3111234cc105a239caee7b1449e75b44d
Amal Nazeem <amalnazeem@gmail.com>
2018-04-12 12:55:04 -0400

    Initial Setup

---
+++ ConvertData.R
@@ -0,0 +1,20 @@
+library(data.table)
+
+fread("./data/mayorsfoodcourt.csv", stringsAsFactors=F) -> food_viola
+head(food_viola)
+
+
+library(magrittr)
+library(plyr)
+
+
+filtered <- dplyr::filter(food_viola, ViolStatus == 'Fail' & Location != '')
+
+df<- ddply(filtered,
+  .(businessName, LegalOwner, NameLast, NameFirst, LICENSENO, Address, CITY, STATE, ZIP, Location),
+  summarise,
+  Severit = length(businessName))
+
+df <- df %>% tidyr::extract(Location, c("lat", "lng"), "\\(([^,]+), ([^)]+)\\)")
+
+write.table(df, file = "./data/FoodViolationData.csv", sep=",",row.names=FALSE)
