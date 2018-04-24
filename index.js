0b6481591b4dbb1d7c3d7d3f68287c30c88ef0de
Amal Nazeem <amalnazeem@gmail.com>
2018-04-23 20:29:24 -0400

    Moved things around because I realized that I can't hide my api key anyway

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var path = require("path")
+var app = express()
+
+app.set('port', (process.env.PORT || 80))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + '/map/index.html'));
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
