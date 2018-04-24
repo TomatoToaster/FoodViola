abeecc68a9bdd5f43ecb3ed398e57f4a1c2019a3
Amal Nazeem <amalnazeem@gmail.com>
2018-04-24 06:08:30 -0400

    Added scoring ability

--- ConvertData.R
+++ ConvertData.R
@@ -1,7 +1,6 @@
 library(data.table)
 
 fread("./data/mayorsfoodcourt.csv", stringsAsFactors=F) -> food_viola
-head(food_viola)
 
 
 library(magrittr)
@@ -9,12 +8,19 @@ library(plyr)
 
 
 filtered <- dplyr::filter(food_viola, ViolStatus == 'Fail' & Location != '')
+filteredp <- dplyr::filter(food_viola, ViolStatus == 'Pass' & Location != '')
 
-df<- ddply(filtered,
-  .(businessName, LegalOwner, NameLast, NameFirst, LICENSENO, Address, CITY, STATE, ZIP, Location),
-  summarise,
-  Severity = length(businessName))
+df <- ddply(filtered,
+            .(businessName, LegalOwner, NameLast, NameFirst, LICENSENO, DESCRIPT, Address, CITY, STATE, ZIP, Location),
+            summarise,
+            Severity = length(businessName))
 
+pf <- ddply(filteredp,
+            .(businessName),
+            summarise,
+            Positivity = length(businessName))
+
+df <- merge(df,pf,by="businessName")
 df <- df %>% tidyr::extract(Location, c("lat", "lng"), "\\(([^,]+), ([^)]+)\\)")
 
 write.table(df, file = "./data/FoodViolationData.csv", sep=",",row.names=FALSE)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A project aiming to help visualize the food inspection situation in Boston, MA.
+
+Best Score = 692
+Worse Score = -176
