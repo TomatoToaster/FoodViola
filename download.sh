9c94e953f1c28a14e67603b083cd0ad84a748872
Amal Nazeem <amalnazeem@gmail.com>
2018-07-18 03:09:47 -0400

    using wget script so daemon can get data periodically from data.boston.gov

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm ./data/mayorsfoodcourt.csv
+wget -O ./data/mayorsfoodcourt.csv "https://data.boston.gov/dataset/03693648-2c62-4a2c-a4ec-48de2ee14e18/resource/4582bec6-2b4f-4f9e-bc55-cbaa73117f4c/download/mayorsfoodcourt.csv"
