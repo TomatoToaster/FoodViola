d44676c1830e67bc8aa57b50fca60d3e81dffc6b
Amal Nazeem <amalnazeem@gmail.com>
2018-10-12 05:36:27 -0400

    moved logic for running R script into sh file

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm ./data/mayorsfoodcourt.csv
 wget -O ./data/mayorsfoodcourt.csv "https://data.boston.gov/dataset/03693648-2c62-4a2c-a4ec-48de2ee14e18/resource/4582bec6-2b4f-4f9e-bc55-cbaa73117f4c/download/mayorsfoodcourt.csv"
+rm ./data/FoodViolationData.csv
+Rscript ConvertData.R
